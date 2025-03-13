9c9697d9f6b3fc660085c05990ccdb817f8212a7
JairEsc <p.escobar816@gmail.com>
2025-03-13 13:57:45 -0600

    Se modifica el estilo de uno de los botones para que solo sea accesible cuando se crea una gráfica

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import dash
 from dash import Dash, html, dcc
+import pandas as pd
+import plotly.graph_objects as go
 
 app = Dash(__name__, use_pages=True)
 
@@ -7,7 +9,53 @@ app.layout = html.Div([
     dash.page_container,
 
 ])
+@app.callback(
+    dash.dependencies.Output("graph", "children"),
+    dash.dependencies.Output("navigate-button", "style"),
+    dash.dependencies.Input("populate_div", "n_clicks"),
+    prevent_initial_call=True
+)
+def populateDiv(click):
+    df_industrial = pd.read_csv("./Balassa_Mod_Nivel_Municipio_por_Grupos_2015.csv")
+    feature = {"properties": {"CVE_MUN": "001", "NOM_MUN": 'Acatlán'}}
+    if not feature:
+        return [html.P("Selecciona un municipio")]
+    nombre = feature["properties"]["CVE_MUN"]
+    row = df_industrial[df_industrial['cve_mun'] == int(nombre)].iloc[:, 2:]
+    if row.empty:
+        return [html.P(f"No data available for {nombre}")]
+    data = row.iloc[0].to_dict()
+    data = {k: (0 if pd.isna(v) else v) for k, v in data.items()}
+    data = dict(sorted(data.items(), key=lambda item: item[1], reverse=True)[:5])
+    x = list(data.keys())
+    y = list(data.values())
 
+    y = [round(val, 2) if val >= 0.01 else "<0.01" for val in y]
+
+    fig = go.Figure(
+        data=go.Bar(
+            x=y,
+            y=x,
+            orientation='h',
+            width=0.5,
+            offset=-0.65,
+            texttemplate="%{x}"
+        ),
+        layout={
+            'title': {
+                'text': f'Industrias con mayor personal en <br> {feature["properties"]["NOM_MUN"]} ()',
+                'font': {'size': 10},
+                'x': 0.5,
+                'xanchor': 'center',
+                'yanchor': 'top',
+            },
+            'height': 300,
+            'yaxis': {'anchor': 'free', 'side': 'right'},
+        }
+    )
+    return [dcc.Graph(figure=fig.update_layout(
+        margin=dict(l=20, r=20, t=30, b=20),
+    ), style={'height': '300px', 'width': '350px'}), {"display": "block"}]
 @app.callback(
     dash.dependencies.Output('url', 'href'),
     [dash.dependencies.Input('navigate-button', 'n_clicks')]
--- pages/home.py
+++ pages/home.py
@@ -1,12 +1,15 @@
 import dash
 from dash import html
 from dash import dcc
+import dash_bootstrap_components as dbc
 
 dash.register_page(__name__, path='/')
-
+info_home=html.Div(children=[html.Div("Grafo",id='graph'),html.Button('Go to not Home', id='navigate-button',style={"display":'none'}),
+    dcc.Location(id='url', refresh=True)],id='div')
 layout = html.Div([
     html.H1('This is our Home page'),
     html.Div('This is our Home page content.'),
-    html.Button('Go to Not Home', id='navigate-button'),
+    html.Button('Populate div. Graph+button', id='populate_div'),
+    info_home,
     dcc.Location(id='url', refresh=True)
 ])
